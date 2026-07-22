d7f1aeab9b2ac1ea42ceaf912af475d7bb0fa07a
tbirddv <terracb1997@gmail.com>
2026-07-21 22:14:17 -0400

    leafnode, untested as yet

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -34,3 +34,17 @@ class HTMLNode:
                     return False
         return (self.tag == other.tag and
                 self.value == other.value)
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=[], props=props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value to convert to HTML.")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
